75ec1dbd617e1afd214431a97b5e26829d2b720e
BishnoiRekha29 <bishnoirekha29@gmail.com>
2025-02-09 22:18:42 +0530

    ios runner

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
+
+test.skip('test', async ({ page }) => {
   await page.goto('https://www.facebook.com/');
   await page.getByTestId('royal_email').click();
   await page.getByTestId('royal_email').fill('rekh@yahoo.co');
