393e8fb0a9399a00cde11de35b24a1c959aed781
BishnoiRekha29 <bishnoirekha29@gmail.com>
2025-02-09 22:21:23 +0530

    commit in featurefeb

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
   await page.goto('https://www.facebook.com/');
-  await page.getByTestId('royal_email').click();
+  //await page.getByTestId('royal_email').click();
   await page.getByTestId('royal_email').fill('rekh@yahoo.co');
   await page.getByTestId('royal_pass').click();
   await page.getByTestId('royal_pass').fill('test123');
